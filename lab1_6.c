c7811492fa6afe859f4e84bbdd4155060f1c17c8
boncpes <78350722+boncpes@users.noreply.github.com>
2021-02-01 18:49:48 +0700

    Initial commit for lab1_6

--- lab1_6.c
+++ lab1_6.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
 int main()
 {
-    int n,m,t;
-    char f;
-    scanf("%d %d %d",&n,&m,&t);
+    int n,m,k=0,i,c=0;
+    signed long int t;
+    char f,ans;
+    scanf("%d %d %li",&n,&m,&t);
+    fflush(stdin);
     scanf("%c",&f);
-
-
+    if(f=='L')
+    {
+        for(i=1; i<t+1; i++)
+        {
+            k=k+n;
+            if(k>t)
+            {
+                ans='L';
+                break;
+            }
+            k=k+m;
+            if(k>t)
+            {
+                ans='R';
+                break;
+            }
+        }
+    }
+    else if(f=='R')
+    {
+        for(i=1; i<t+1; i++)
+        {
+            k=k+m;
+            if(k>t)
+            {
+                ans='R';
+                break;
+            }
+            k=k+n;
+            if(k>t)
+            {
+                ans='L';
+                break;
+            }
+        }
+    }
+    printf("%c",ans);
     return 0;
 }
